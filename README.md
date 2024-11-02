5565ae6dd8cac225c79dc7c32199e3cca0c4b038
Caspar <caspar@kodekitchen.com>
2024-11-02 16:56:23 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# rp2040_lily
+# Lily58 with rp2040 promicro compatible controller and KMK
+
+![Keyboard](./pics/keyboard.png)
+
+## Purpose
+
+I wanted to use an rp2040 in a [lily58](https://github.com/kata0510/Lily58) to be able to use [kmk](https://github.com/KMKfw) on a lily build already working with QMK. 
+The idea was to just swap the controller and try a different software, I did not want to plan and/or order all new PCBs around Picos, and rp2040 promicro controllers are cheap to get.
+
+
+## Parts
+
+- an assembled lily58
+- a promirco drop-in rp2040 board, I used [Tenstar RP2040 Pro Micro Development Board 4MB/16MB Dual-Core](https://de.aliexpress.com/item/1005006599393967.html?gatewayAdapt=glo2deu)
+- trrs cable (trs will work with the atmega 32u4 but not the rp2040s)
+- solder the controllers onto the pin headers exactly as with the regular promicros (chips down)
+
+
+## Not covered
+
+I have no RGB and no rotary encoders, so that is not covered.
+
+
+## Setup
+
+Using [circuitpython sparkfun pro micro rp2040]https://circuitpython.org/board/sparkfun_pro_micro_rp2040/ for circuitpython worked.
+
+Install it by downloading and drag and drop it to the controller showing up as a usb device (RPI-RP2) - if not hold the BOOT or BOOTSEL button while plugging the usb cable in and releasing it. 
+After copying the file the device will automatically vanish and reappear as CIRCUITPY (if not try several times, they might be a little stubborn at times).
+
+Then install KMK (drag and drop the kmk folder and boot.py onto the device).
+
+At this point it is a good idea to download [picocom](https://github.com/npat-efault/picocom) and access the device from it (for me it was ./picocom /dev/ttyACM0 on ubuntu) so you can debug stuff.
+
+It is a good idea to name the two controllers differently for left and right to not get confused (https://learn.adafruit.com/welcome-to-circuitpython/renaming-circuitpy) - ideally you want the names to end with L or R respectively to not have to configure it in the source code.
+
+For the keyboard to work you will basically need two files:
+
+	1. kb.py, defining the layout and pins
+	2. code.py, the keymap and configuration (split, media keys etc.)
+
+
+## Figure out pins for other boards
+
+https://learn.adafruit.com/circuitpython-essentials/circuitpython-pins-and-modules has a nice script to get the names correct if you got a pinout schematic for your board.
---
+++ code.py
@@ -0,0 +1,61 @@
+from kb import KMKKeyboard
+
+from kmk.extensions.media_keys import MediaKeys
+
+from kmk.keys import KC
+from kmk.modules.holdtap import HoldTap
+from kmk.modules.layers import Layers
+from kmk.modules.split import Split
+
+
+keyboard = KMKKeyboard()
+# keyboard.debug_enabled = True
+
+# Adding modules
+# Using drive names (LILY58L, LILY58R) to recognize sides; use split_side arg if you're not doing it
+split = Split(
+    split_flip=False,
+    use_pio=True,
+)
+
+keyboard.modules = [split, Layer(), HoldTap()]
+
+keyboard.extensions = [MediaKeys()]
+
+# Cleaner key names
+_______ = KC.TRNS
+XXXXXXX = KC.NO
+
+
+LOWER = KC.MO(1)
+RAISE = KC.MO(2)
+
+
+# fmt:off
+keyboard.keymap = [
+    [  #QWERTY
+        KC.ESC,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                          KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.MINS,
+        KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                           KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.LBRC,
+        KC.LCTL, KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                           KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOT,
+        KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.ENTER,     KC.BSPC, KC.N,    KC.M,    KC.COMM, KC.DOT,  KC.SLSH, KC.RSFT,
+                                   KC.LALT, KC.LCTL, LOWER,   KC,SPC,       KC.SPC,  RAISE,  KC.LGUI,  KC.RALT,
+    ],
+    [   #LOWER
+        _______, KC.MUTE,       KC.VOLU,       KC.VOLD,        XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, KC.RALT(KC.Q), XXXXXXX,       KC.RALT(KC.E),  RPAR,    KC.PLUS,                        XXXXXXX, KC.KP_7, KC.KP_8, KC.KP_9, XXXXXXX, KC.F12,
+        _______, KC.GRAVE,      KC.SLSH(KC_1), KC.LBRC,        KC.RBRC, KC.EQL,                         XXXXXXX, KC.KP_4, KC.KP_5, KC.KP_6, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX,       XXXXXXX,       LGT,            RGT,     PIPE,    AMP,          XXXXXXX, KC.KP_0, KC.KP_1, KC.KP_2, KC.KP_3, KC.BSLS, XXXXXXX,
+                                                      _______, _______, _______, _______,      _______, _______, _______, _______,
+    ],
+    [   #RAISE
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        MENU,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, KC.UP,   XXXXXXX, XXXXXXX, XXXXXXX,                        KC.VOLU, KC.HOME, KC.UP,   KC.END,  KC.PGUP, XXXXXXX,
+        _______, KC.LEFT, KC.DOWN, KC.RGHT, XXXXXXX, XXXXXXX,                        KC.VOLD, KC.LEFT, KC.DOWN, KC.RGHT, KC.PGDN, XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,      _______, KC.MPLY, KC.INS,  KC.DEL,  XXXXXXX, XXXXXXX, XXXXXXX,
+                                   _______, _______, _______, _______,      _______, _______, _______, _______,
+    ]
+]
+# fmt:on
+
+if __name__ == '__main__':
+    keyboard.go()
